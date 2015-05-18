5461ae0802e577ce93ada8ecbc3b40a20737c3ab
Lisachenko Alexander <lisachenko.it@gmail.com>
2015-05-18 23:06:09 +0300

    Add automatic language injection for PointcutBuilder class methods

--- src/com/aopphp/go/injector/PointcutQueryLanguageInjector.java
+++ src/com/aopphp/go/injector/PointcutQueryLanguageInjector.java
@@ -4,11 +4,15 @@ import com.aopphp.go.PointcutQueryLanguage;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.InjectedLanguagePlaces;
 import com.intellij.psi.LanguageInjector;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLanguageInjectionHost;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.php.lang.documentation.phpdoc.psi.tags.PhpDocTag;
+import com.jetbrains.php.lang.psi.elements.MethodReference;
+import com.jetbrains.php.lang.psi.elements.ParameterList;
 import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import com.jetbrains.php.lang.psi.elements.impl.MethodImpl;
 import de.espend.idea.php.annotation.dict.PhpDocTagAnnotation;
 import de.espend.idea.php.annotation.pattern.AnnotationPattern;
 import de.espend.idea.php.annotation.util.AnnotationUtil;
@@ -28,11 +32,54 @@ public class PointcutQueryLanguageInjector implements LanguageInjector {
             return;
         }
 
+        boolean enableInjection = isInjectionIntoAnnotation(host);
+        enableInjection |= isInjectionIntoPointcutBuilder(host);
+
+        if (enableInjection) {
+            TextRange rangeInsideHost = new TextRange(1, host.getTextLength() - 1);
+            injectionPlacesRegistrar.addPlace(PointcutQueryLanguage.INSTANCE, rangeInsideHost, "", "");
+        }
+    }
+
+    /**
+     * Checks if injection is performed into PointcutBuilder->method('<pointcutExpression>', function () {...})
+     * @param host Host element is typically PHP string
+     *
+     * @return boolean
+     */
+    private boolean isInjectionIntoPointcutBuilder(PsiLanguageInjectionHost host)
+    {
+        PsiElement element = host.getParent();
+        if (!(element instanceof ParameterList)) {
+            return false;
+        }
+        element = element.getParent();
+        if (!(element instanceof MethodReference)) {
+            return false;
+        }
+
+        MethodImpl methodImpl = (MethodImpl) ((MethodReference) element).resolve();
+        if (methodImpl == null || methodImpl.getFQN() == null) {
+            return false;
+        }
+
+        return methodImpl.getFQN().startsWith("\\Go\\Aop\\Support\\PointcutBuilder");
+    }
+
+    /**
+     * Checks if injection is performed into annotation
+     *
+     * @param host Host element, typically string literal expression
+     *
+     * @return boolean
+     */
+    private boolean isInjectionIntoAnnotation (@NotNull PsiLanguageInjectionHost host)
+    {
         boolean enableInjection = false;
 
         PhpDocTag phpDocTag = PsiTreeUtil.getParentOfType(host, PhpDocTag.class);
         if (phpDocTag == null || !AnnotationPattern.getDocBlockTag().accepts(phpDocTag)) {
-            return;
+            return false;
         }
 
         PhpDocTagAnnotation phpDocAnnotationContainer = AnnotationUtil.getPhpDocAnnotationContainer(phpDocTag);
@@ -42,16 +89,13 @@ public class PointcutQueryLanguageInjector implements LanguageInjector {
         }
 
         if (phpClass == null || phpClass.getFQN() == null) {
-            return;
+            return false;
         }
 
         if (phpClass.getFQN().startsWith(GO_AOP_ANNOTATION)) {
             enableInjection = true;
         }
 
-        if (enableInjection) {
-            TextRange rangeInsideHost = new TextRange(1, host.getTextLength() - 1);
-            injectionPlacesRegistrar.addPlace(PointcutQueryLanguage.INSTANCE, rangeInsideHost, "", "");
-        }
+        return enableInjection;
     }
 }
