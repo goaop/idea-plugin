180367a3890aa268e21e0e06648d0286ac047426
Alexander Lisachenko <lisachenko.it@gmail.com>
2015-05-29 17:38:53 +0300

    [WIP] Add stub index for annotated pointcuts #3

---
+++ src/com/aopphp/go/stubs/indexes/AnnotationPointcutExpressionStubIndex.java
@@ -0,0 +1,129 @@
+package com.aopphp.go.stubs.indexes;
+
+import com.aopphp.go.psi.impl.PointcutQueryUtil;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiRecursiveElementWalkingVisitor;
+import com.intellij.util.indexing.*;
+import com.intellij.util.io.DataExternalizer;
+import com.intellij.util.io.EnumeratorStringDescriptor;
+import com.intellij.util.io.KeyDescriptor;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.tags.PhpDocTag;
+import com.jetbrains.php.lang.psi.PhpFile;
+import com.jetbrains.php.lang.psi.stubs.indexes.PhpConstantNameIndex;
+import gnu.trove.THashMap;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Map;
+
+public class AnnotationPointcutExpressionStubIndex extends FileBasedIndexExtension<String, String> {
+
+    public static final ID<String, String> KEY = ID.create("com.aopphp.go.annotation.pointcuts");
+    private final KeyDescriptor<String> myKeyDescriptor = new EnumeratorStringDescriptor();
+
+    @NotNull
+    @Override
+    public ID<String, String> getName() {
+        return KEY;
+    }
+
+    @NotNull
+    @Override
+    public DataIndexer<String, String, FileContent> getIndexer() {
+        return new DataIndexer<String, String, FileContent>() {
+            @NotNull
+            @Override
+            public Map<String, String> map(@NotNull FileContent inputData) {
+                final Map<String, String> map = new THashMap<String, String>();
+
+                PsiFile psiFile = inputData.getPsiFile();
+                if(!(psiFile instanceof PhpFile)) {
+                    return map;
+                }
+
+                psiFile.accept(new DocTagRecursiveElementVisitor(map));
+
+                return map;
+            }
+        };
+    }
+
+    @NotNull
+    @Override
+    public KeyDescriptor<String> getKeyDescriptor() {
+        return this.myKeyDescriptor;
+    }
+
+    @NotNull
+    @Override
+    public DataExternalizer<String> getValueExternalizer() {
+        return new DataExternalizer<String>() {
+            @Override
+            public void save(@NotNull DataOutput out, String value) throws IOException {
+                out.writeUTF(value);
+            }
+
+            @Override
+            public String read(@NotNull DataInput in) throws IOException {
+                return in.readUTF();
+            }
+        };
+    }
+
+    @NotNull
+    @Override
+    public FileBasedIndex.InputFilter getInputFilter() {
+        return PhpConstantNameIndex.PHP_INPUT_FILTER;
+    }
+
+    @Override
+    public boolean dependsOnFileContent() {
+        return true;
+    }
+
+    @Override
+    public int getVersion() {
+        return 1;
+    }
+
+    private static class DocTagRecursiveElementVisitor extends PsiRecursiveElementWalkingVisitor {
+
+        private final Map<String, String> map;
+        private Map<String, String> fileImports;
+
+        public DocTagRecursiveElementVisitor(Map<String, String> map) {
+            this.map = map;
+        }
+
+        @Override
+        public void visitElement(PsiElement element) {
+            if ((element instanceof PhpDocTag)) {
+                visitPhpDocTag((PhpDocTag) element);
+            }
+            super.visitElement(element);
+        }
+
+        public void visitPhpDocTag(PhpDocTag phpDocTag) {
+
+            // init file imports
+            if(this.fileImports == null) {
+                this.fileImports = PointcutQueryUtil.getFileUseImports(phpDocTag.getContainingFile());
+            }
+
+            if(this.fileImports.size() == 0) {
+                return;
+            }
+
+            String annotationFqnName = PointcutQueryUtil.getClassNameReference(phpDocTag, this.fileImports);
+            if(annotationFqnName != null && annotationFqnName.startsWith("\\Go\\Lang\\Annotation\\")) {
+                map.put(phpDocTag.getText(), annotationFqnName);
+                // TODO: extract and store pointcut expressions
+            }
+        }
+
+    }
+}
+
