3a59ae8892c61c7620a9d09ee2cd9665e14f63db
Lisachenko Alexander <lisachenko.it@gmail.com>
2016-03-21 21:56:23 +0300

    Added ClassMemberReference class implementation

---
+++ src/com/aopphp/go/pointcut/ClassMemberReference.java
@@ -0,0 +1,41 @@
+package com.aopphp.go.pointcut;
+
+/**
+ * Data transfer object for storing a reference to the class member (property or method)
+ */
+public class ClassMemberReference {
+
+    private final PointFilter classFilter;
+    private final MemberAccessMatcherFilter visibilityFilter;
+    private final MemberStateMatcherFilter accessTypeFilter;
+    private final String memberNamePattern;
+
+    public ClassMemberReference(
+            PointFilter classFilter,
+            MemberAccessMatcherFilter visibilityFilter,
+            MemberStateMatcherFilter accessTypeFilter,
+            String memberNamePattern
+    ) {
+
+        this.classFilter       = classFilter;
+        this.visibilityFilter  = visibilityFilter;
+        this.accessTypeFilter  = accessTypeFilter;
+        this.memberNamePattern = memberNamePattern;
+    }
+
+    public PointFilter getClassFilter() {
+        return classFilter;
+    }
+
+    public MemberAccessMatcherFilter getVisibilityFilter() {
+        return visibilityFilter;
+    }
+
+    public MemberStateMatcherFilter getAccessTypeFilter() {
+        return accessTypeFilter;
+    }
+
+    public String getMemberNamePattern() {
+        return memberNamePattern;
+    }
+}
