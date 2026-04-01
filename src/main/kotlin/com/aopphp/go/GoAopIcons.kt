4b2423731b3dbf5375c19444fa8ac8344b1f89c1
Alexander Lisachenko <lisachenko.it@gmail.com>
2026-04-02 00:42:12 +0300

    feat: Use SVG icons for the UI to have better UI

--- src/main/kotlin/com/aopphp/go/GoAopIcons.kt
+++ src/main/kotlin/com/aopphp/go/GoAopIcons.kt
@@ -3,10 +3,10 @@ package com.aopphp.go
 import com.intellij.openapi.util.IconLoader
 
 object GoAopIcons {
-    @JvmField val FILE         = IconLoader.getIcon("/com/aopphp/go/icons/aspect.png", GoAopIcons::class.java)
-    @JvmField val ASPECT       = IconLoader.getIcon("/com/aopphp/go/icons/aspect.png", GoAopIcons::class.java)
-    @JvmField val ADVISING_ELEMENT = IconLoader.getIcon("/com/aopphp/go/icons/advisingElement.png", GoAopIcons::class.java)
-    @JvmField val ADVISED_ELEMENT  = IconLoader.getIcon("/com/aopphp/go/icons/advisedElement.png", GoAopIcons::class.java)
-    @JvmField val POINTCUT     = IconLoader.getIcon("/com/aopphp/go/icons/pointcut.png", GoAopIcons::class.java)
-    @JvmField val INTRODUCTION = IconLoader.getIcon("/com/aopphp/go/icons/introduction.png", GoAopIcons::class.java)
+    @JvmField val FILE             = IconLoader.getIcon("/com/aopphp/go/icons/pointcut.svg", GoAopIcons::class.java)
+    @JvmField val ASPECT           = IconLoader.getIcon("/com/aopphp/go/icons/aspect.svg", GoAopIcons::class.java)
+    @JvmField val ADVISING_ELEMENT = IconLoader.getIcon("/com/aopphp/go/icons/advisingElement.svg", GoAopIcons::class.java)
+    @JvmField val ADVISED_ELEMENT  = IconLoader.getIcon("/com/aopphp/go/icons/advisedElement.svg", GoAopIcons::class.java)
+    @JvmField val POINTCUT         = IconLoader.getIcon("/com/aopphp/go/icons/pointcut.svg", GoAopIcons::class.java)
+    @JvmField val INTRODUCTION     = IconLoader.getIcon("/com/aopphp/go/icons/introduction.svg", GoAopIcons::class.java)
 }
